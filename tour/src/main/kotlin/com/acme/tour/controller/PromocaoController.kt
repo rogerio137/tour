03cc2e97764173c766609898d3a5e2a85ca8d666
Rogerio <rogerio.137@bol.com.br>
2020-09-09 11:00:06 -0300

    Criando map de promocoes

--- tour/src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
+++ tour/src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -4,17 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import com.acme.tour.model.Promocao
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class PromocaoController{
+
+    @Autowired
+    lateinit  var promocoes: ConcurrentHashMap<Long, Promocao>
 
     @RequestMapping(value= ["/stayHello"], method = arrayOf(RequestMethod.GET))
     fun sayHello(): String{
         return "Hello World"
     }
     @RequestMapping(value= ["/promocoes"], method = arrayOf(RequestMethod.GET))
-    fun getPromocao(): Promocao{
-        val promocao = Promocao(1, "Maravilhosa viagem a cacum", "Cancum", true, 7, 4200.99)
-        return promocao
-    }
+    fun getPromocao() = promocoes[3]
 }
